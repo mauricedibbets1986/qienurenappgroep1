f3c86061ccfb7215e142e7e2bb23dfeed785af90
mauricedibbets1986 <mauricedibbets1986@gmail.com>
2020-07-28 19:10:32 +0200

    start project

---
+++ qienuren-backend/src/main/java/app/qienuren/QienurenApplication.java
@@ -0,0 +1,15 @@
+package app.qienuren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QienurenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QienurenApplication.class, args);
+		//regel hieronder niet aanpassen aub
+		System.out.println("Uren App groep 1. It's Alive!");
+	}
+
+}
---
+++ qienuren-backend/src/test/java/app/qienuren/QienurenApplicationTests.java
@@ -0,0 +1,13 @@
+package app.qienuren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QienurenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
