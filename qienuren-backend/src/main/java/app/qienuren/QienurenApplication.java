471c65ae9efc325208839f7ea70c84ff7632b404
Cecilia <c.a.vanmierlo@gmail.com>
2020-08-04 16:48:24 +0200

    Cecilia

--- qienuren-backend/src/main/java/app/qienuren/QienurenApplication.java
+++ qienuren-backend/src/main/java/app/qienuren/QienurenApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QienurenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QienurenApplication.class, args);
-		//regel hieronder niet aanpassen aub
-		System.out.println("Uren App groep 1. It's Alive!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QienurenApplication.class, args);
+        //regel hieronder niet aanpassen aub
+        System.out.println("Uren App groep 1. It's Alive!");
+    }
 
 }
+ 
