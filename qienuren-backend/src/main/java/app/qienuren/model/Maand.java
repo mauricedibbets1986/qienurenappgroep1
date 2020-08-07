8300dc6c1a466384bc7ef32bffe9005872d98e9d
Joroovb <jorisvanbreugel@gmail.com>
2020-08-07 10:03:49 +0200

    - Changed Enum type to String

---
+++ qienuren-backend/src/main/java/app/qienuren/model/Maand.java
@@ -0,0 +1,16 @@
+package app.qienuren.model;
+
+public enum Maand {
+    JANUARI,
+    FEBRUARI,
+    MAART,
+    APRIL,
+    MEI,
+    JUNI,
+    JULI,
+    AUGUSTUS,
+    SEPTEMBER,
+    OKTOBER,
+    NOVEMBER,
+    DECEMBER
+}
