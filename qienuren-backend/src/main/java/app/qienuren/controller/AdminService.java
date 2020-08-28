88cb775444b1faf65263c5bf711e704f904826f7
Joroovb <jorisvanbreugel@gmail.com>
2020-08-28 16:05:18 +0200

    Update AdminService.java

--- qienuren-backend/src/main/java/app/qienuren/controller/AdminService.java
+++ qienuren-backend/src/main/java/app/qienuren/controller/AdminService.java
@@ -5,11 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-<<<<<<< HEAD
-public class AdminService {}
-=======
+
 public class AdminService {
 
     }
-
->>>>>>> master
