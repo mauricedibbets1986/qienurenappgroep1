8f9a2e5a8c51abb761591e293bb8175f4d2e90b7
Bob-Coding <bobgunsing@gmail.com>
2020-09-02 01:18:37 +0200

    update logout en admin controle

--- qienuren-frontend/js/javascript.js
+++ qienuren-frontend/js/javascript.js
@@ -1,35 +1,54 @@
 function gebruikersnaam() {
-    var naam = "";
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (this.readyState == 4) {
-        var obj = JSON.parse(this.responseText);
-        voornaam = "Hallo " + obj.voornaam + "!";
-        naam = obj.voornaam + " " + obj.achternaam;
-      }
-
-      document.getElementById("gebruikersnaam").innerHTML = naam;
-      document.getElementById("gebruikersnaam2").innerHTML = voornaam;
-
+  var naam = "";
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      var obj = JSON.parse(this.responseText);
+      voornaam = "Hallo " + obj.voornaam + "!";
+      naam = obj.voornaam + " " + obj.achternaam;
     }
-    xhr.open("GET", "http://173.212.208.199:1337/api/users/" + localStorage.getItem("userId"), true);
-    xhr.setRequestHeader("Authorization", localStorage.getItem("token"));
-    xhr.send();
-  }
 
-  function bedrijfsnaam() {
-    var naam = "";
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (this.readyState == 4) {
-        var obj = JSON.parse(this.responseText);
-        naam = obj.bedrijfsNaam;
-      }
+    document.getElementById("gebruikersnaam").innerHTML = naam;
+    document.getElementById("gebruikersnaam2").innerHTML = voornaam;
+  };
+  xhr.open(
+    "GET",
+    "http://173.212.208.199:1337/api/users/" + localStorage.getItem("userId"),
+    true
+  );
+  xhr.setRequestHeader("Authorization", localStorage.getItem("token"));
+  xhr.send();
+}
 
-      document.getElementById("bedrijfsnaam").innerHTML = naam;
-
+function bedrijfsnaam() {
+  var naam = "";
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      var obj = JSON.parse(this.responseText);
+      naam = obj.bedrijfsNaam;
     }
-    xhr.open("GET", "http://173.212.208.199:1337/api/users/bedrijf/" + localStorage.getItem("userId"), true);
-    xhr.setRequestHeader("Authorization", localStorage.getItem("token"));
-    xhr.send();
-  }
+
+    document.getElementById("bedrijfsnaam").innerHTML = naam;
+  };
+  xhr.open(
+    "GET",
+    "http://173.212.208.199:1337/api/users/bedrijf/" +
+      localStorage.getItem("userId"),
+    true
+  );
+  xhr.setRequestHeader("Authorization", localStorage.getItem("token"));
+  xhr.send();
+}
+
+const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("userRole");
+  window.location.href = "../inlogscherm.html";
+};
+
+const loginAuthAdmin = () => {
+  if (localStorage.getItem("userRole") != "[ROLE_ADMIN]")
+    window.location.href = "../inlogscherm.html";
+};
