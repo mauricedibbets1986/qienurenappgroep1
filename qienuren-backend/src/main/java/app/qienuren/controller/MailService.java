f2216e5db0404c111f0898e2f1eb4071cde56457
Joroovb <jorisvanbreugel@gmail.com>
2020-09-04 15:47:05 +0200

    beter werkt het

--- qienuren-backend/src/main/java/app/qienuren/controller/MailService.java
+++ qienuren-backend/src/main/java/app/qienuren/controller/MailService.java
@@ -2,10 +2,14 @@ package app.qienuren.controller;
 
 import app.qienuren.model.Mail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Service
 public class MailService {
@@ -32,4 +38,5 @@ public class MailService {
         msg.setText(mail.getText());
         javaMailSender.send(msg);
     }
+
 }
